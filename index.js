6da7bc6855c7251eb0fdff274c3da182611f5613
HaBang30 <doanduytung1602@gmail.com>
2023-04-26 07:55:06 +0700

    json-ajax

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const animals = [
+    {
+        "name": "Whiskers",
+        "species": "cat",
+        'food': {
+            "like": ["celery", "strawberries"],
+            "dislike": ["carrots"]
+        }
+    },
+    {
+        "name" : "Woof",
+        "species" : "dog",
+        "food" : {
+            "like" : ["dog food"],
+            "dislike" : ["Cat food"]
+        }
+    },
+    {
+        "name" : "Fluffy",
+        "species" : "cat",
+        "food" : {
+            "like" : ["cannel food"],
+            "dislike" : ["dry food"]
+        }
+    }
+]
+console.log(animals);
